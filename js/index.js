3b3c8126afc96fe0296c45a7982c6b2bc1c411c3
BY021 <baker.yousif@chasacademy.com>
2026-06-25 00:27:01 +0200

    path issue

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ async function getData()
 {
   try 
   {
-    const response = await fetch("../js/index.json");
+    const response = await fetch("/js/index.json");
     if(!response.ok)
     {
       throw new Error(`HTTP ERROR status: ${response.status}`);
